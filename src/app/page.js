0312bcd0e723e511aaa7aef9b2e44eb8d735244d
Hannah Zong <yanhuazong@gmail.com>
2025-11-12 22:44:28 -0500

    Seperate form in a component

--- src/app/page.js
+++ src/app/page.js
@@ -33,36 +33,32 @@ export default async function Home({ searchParams }) {
   ]);
 
   return (
-    <main className={styles.main}>
-      <div className="section">
-        <div className="container">
-          <h1>Profile App </h1>
-          <Filters titles={titles} title={selectedTitle} search={search} />
-          {profiles.length === 0 ? (
-            <p>No profiles found.</p>
-          ) : (
-            <div className="grid">
-              {profiles.map((profile) => (
-                <Link key={profile.id} href={`/profile/${profile.id}`}>
-                  <div className={styles["profile-card"]}>
-                    <div className={styles["profile-card__image"]}>
-                      <img
-                        src={profile.image_url || "/vercel.svg"}
-                        alt={profile.name}
-                      />
-                    </div>
-                    <div className={styles["profile-card__content"]}>
-                      <p>{profile.name}</p>
-                      <p>{profile.title}</p>
-                      <p>{profile.email}</p>
-                    </div>
-                  </div>
-                </Link>
-              ))}
-            </div>
-          )}
+    <>
+      <h1>Profile App </h1>
+      <Filters titles={titles} title={selectedTitle} search={search} />
+      {profiles.length === 0 ? (
+        <p>No profiles found.</p>
+      ) : (
+        <div className="grid">
+          {profiles.map((profile) => (
+            <Link key={profile.id} href={`/profile/${profile.id}`}>
+              <div className={styles["profile-card"]}>
+                <div className={styles["profile-card__image"]}>
+                  <img
+                    src={profile.image_url || "/vercel.svg"}
+                    alt={profile.name}
+                  />
+                </div>
+                <div className={styles["profile-card__content"]}>
+                  <p>{profile.name}</p>
+                  <p>{profile.title}</p>
+                  <p>{profile.email}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
         </div>
-      </div>
-    </main>
+      )}
+    </>
   );
 }
