a1bfbe3b4d90dbd9f7052dd2184761971253bc91
Hannah Zong <yanhuazong@gmail.com>
2025-11-17 14:22:34 -0500

    Split auth

---
+++ src/auth-edge.js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+
+export const { auth } = NextAuth({
+  pages: {
+    signIn: "/auth/signin",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = auth && auth.user;
+      const path = nextUrl.pathname;
+      const isProtectedRoute =
+        path.startsWith("/add-profile") ||
+        (path.startsWith("/profile/") && path.endsWith("/edit"));
+      if (isProtectedRoute && !isLoggedIn) {
+        return false; // Redirect to sign-in page
+      }
+      return true;
+    },
+  },
+});
